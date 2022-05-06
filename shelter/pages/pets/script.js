067093604530020d10931c4a0c8a2ba98ad5c23d
Aleksandr <alex-davydov211@mail.ru>
2022-05-06 17:13:27 +0300

    feat: add burger

---
+++ shelter/pages/pets/script.js
@@ -0,0 +1,13 @@
+
+
+const BURGER_MENU = document.querySelector(".hamburger");
+const OVERLAY = document.querySelector(".overlay");
+const NAV = document.querySelector(".navigation");
+const BODY = document.querySelector("body");
+
+BURGER_MENU.addEventListener("click", () => {
+    BURGER_MENU.classList.toggle("open"), 
+    OVERLAY.classList.toggle("active"), 
+    NAV.classList.toggle("open")
+    BODY.classList.toggle("hiddenScroll")
+});
