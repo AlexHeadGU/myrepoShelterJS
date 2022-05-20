7524250ff3084d726c4f3f30826553f22ca022d6
Aleksandr <alex-davydov211@mail.ru>
2022-05-20 16:42:10 +0300

    fix: slider

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -6,24 +6,35 @@ const BTN_RIGHT = document.querySelector("#btn-right");
 const CAROUSEL = document.querySelector("#carousel");
 const ITEM_LEFT = document.querySelector("#item-left");
 const ITEM_RIGHT = document.querySelector("#item-right");
+const ITEM_ACTIVE = document.querySelector("#item-active");
 
 
 const moveLeft = () => {
     CAROUSEL.classList.add("transition-left");
+    createCardTemplate();
     BTN_LEFT.removeEventListener("click", moveLeft);
     BTN_RIGHT.removeEventListener("click", moveRight);
 };
 
 const moveRight = () => {
     CAROUSEL.classList.add("transition-right");
+    createCardTemplate();
     BTN_LEFT.removeEventListener("click", moveLeft);
     BTN_RIGHT.removeEventListener("click", moveRight);
 };
 
 const createCardTemplate = () => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    return card;
+    for(let i = 0; i < 3; i++){
+        const img = ITEM_LEFT.querySelectorAll("img");
+        const p = ITEM_LEFT.querySelectorAll("p");
+       
+        for(let i = 0; i < img.length; i++){
+            let j = Math.floor(Math.random() * pets.length);
+            img[i].src = `../../assets/images/${pets[j].name.toLowerCase()}.png`;
+            img[i].alt = pets[j].name.toLowerCase();
+            p[i].innerHTML = pets[j].name;
+        }
+    }
 };
 
 const names = [];
@@ -46,38 +57,19 @@ CAROUSEL.addEventListener("animationend", (animationEvent) => {
         CAROUSEL.classList.remove("transition-left");
         changedItem = ITEM_LEFT;
         document.querySelector("#item-active").innerHTML = ITEM_LEFT.innerHTML;
+        let elem = document.querySelector("#item-active")
+        var matches = elem.querySelectorAll("div.card");
+
+        for (let i = 0; i < matches.length; i++){
+            matches[i].classList.remove("cards-left"); 
+        }
+
     }else{
         CAROUSEL.classList.remove("transition-right");
         changedItem = ITEM_RIGHT;
         document.querySelector("#item-active").innerHTML = ITEM_RIGHT.innerHTML;
         };
 
-    changedItem.innerHTML = "";
-
-    for(let i = 0; i < 3; i++){
-        const card = createCardTemplate();
-        const img = document.createElement("img");
-        const p = document.createElement("p");
-        const btn = document.createElement("button");
-        card.appendChild(img);
-        card.appendChild(p);
-        card.appendChild(btn);
-        let j = Math.floor(Math.random() * pets.length);
-        img.src = `../../assets/images/${pets[j].name.toLowerCase()}.png`;
-        img.alt = pets[j].name.toLowerCase();
-        p.innerHTML = pets[j].name;
-        p.classList.add("pet-name");
-        btn.innerHTML = "Learn more";
-        btn.classList.add("cardBtn")
-        changedItem.appendChild(card);
-    }
-
-
-    // for(let i = 0; i < 3; i++){
-    //     const card = createCardTemplate();
-    //     card.innerText = Math.floor(Math.random() * 8);
-    //     changedItem.appendChild(card);
-    // };
     
     BTN_LEFT.addEventListener("click", moveLeft);
     BTN_RIGHT.addEventListener("click", moveRight);
