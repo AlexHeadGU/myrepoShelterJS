d506247ea2590c41e25def307c842fd9c1d07bec
Alexandr <alex-davydov211@mail.ru>
2022-05-22 16:56:54 +0300

    fix: remove repeat img in slider

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -7,37 +7,55 @@ const CAROUSEL = document.querySelector("#carousel");
 const ITEM_LEFT = document.querySelector("#item-left");
 const ITEM_RIGHT = document.querySelector("#item-right");
 const ITEM_ACTIVE = document.querySelector("#item-active");
+const names = [];
 
+const createCardTemplate = (direction) => {
+    let img;
+    let p;
+    let newArr = ITEM_ACTIVE.querySelectorAll("p");
+    let activeArr = [];
+
+    for(let i = 0; i < newArr.length; i++){
+        activeArr.push(newArr[i].textContent);
+    }
+
+    for(let i = 0; i < 3; i++){
+        if(direction === "left"){
+            img = ITEM_LEFT.querySelectorAll("img");
+            p = ITEM_LEFT.querySelectorAll("p");
+        }else{
+            img = ITEM_RIGHT.querySelectorAll("img");
+            p = ITEM_RIGHT.querySelectorAll("p");
+        }
+
+        for(let k = 0; k < 1; k++){
+            let j = Math.floor(Math.random() * pets.length);
+
+            if(activeArr.includes(pets[j].name)){
+                k--;
+            }else{
+                img[i].src = `../../assets/images/${pets[j].name.toLowerCase()}.png`;
+                img[i].alt = pets[j].name.toLowerCase();
+                p[i].innerHTML = pets[j].name;
+                activeArr.push(pets[j].name);
+            }
+        }
+    }
+};
 
 const moveLeft = () => {
     CAROUSEL.classList.add("transition-left");
-    createCardTemplate();
+    createCardTemplate("left");
     BTN_LEFT.removeEventListener("click", moveLeft);
     BTN_RIGHT.removeEventListener("click", moveRight);
 };
 
 const moveRight = () => {
     CAROUSEL.classList.add("transition-right");
-    createCardTemplate();
+    createCardTemplate("right");
     BTN_LEFT.removeEventListener("click", moveLeft);
     BTN_RIGHT.removeEventListener("click", moveRight);
 };
-
-const createCardTemplate = () => {
-    for(let i = 0; i < 3; i++){
-        const img = ITEM_LEFT.querySelectorAll("img");
-        const p = ITEM_LEFT.querySelectorAll("p");
-       
-        for(let i = 0; i < img.length; i++){
-            let j = Math.floor(Math.random() * pets.length);
-            img[i].src = `../../assets/images/${pets[j].name.toLowerCase()}.png`;
-            img[i].alt = pets[j].name.toLowerCase();
-            p[i].innerHTML = pets[j].name;
-        }
-    }
-};
-
-const names = [];
 
 for(let i = 0; i < pets.length; i++){
   if(!names.includes(pets[i].name)){
@@ -45,31 +63,34 @@ for(let i = 0; i < pets.length; i++){
   }
 };
 
-console.log(names);
-
 BTN_LEFT.addEventListener("click", moveLeft);
 BTN_RIGHT.addEventListener("click", moveRight);
 
-CAROUSEL.addEventListener("animationend", (animationEvent) => {
-    console.log(animationEvent);
-    let changedItem;
-    if(animationEvent.animationName === "move-left"){
-        CAROUSEL.classList.remove("transition-left");
-        changedItem = ITEM_LEFT;
+const REMOVE_CLASS = (direction) => {
+    CAROUSEL.classList.remove(`transition-${direction}`);
+
+    if(direction==="left"){
+        let changedItem = ITEM_LEFT;
         document.querySelector("#item-active").innerHTML = ITEM_LEFT.innerHTML;
-        let elem = document.querySelector("#item-active")
-        var matches = elem.querySelectorAll("div.card");
-
-        for (let i = 0; i < matches.length; i++){
-            matches[i].classList.remove("cards-left"); 
-        }
-
     }else{
-        CAROUSEL.classList.remove("transition-right");
-        changedItem = ITEM_RIGHT;
+        let changedItem = ITEM_RIGHT;
         document.querySelector("#item-active").innerHTML = ITEM_RIGHT.innerHTML;
-        };
+    }
+    
+    let elem = document.querySelector("#item-active")
+    var matches = elem.querySelectorAll("div.card");
 
+    for (let i = 0; i < matches.length; i++){
+        matches[i].classList.remove(`cards-${direction}`); 
+    }
+}
+
+CAROUSEL.addEventListener("animationend", (animationEvent) => {
+    if(animationEvent.animationName === "move-left"){
+        REMOVE_CLASS("left");
+    }else{
+        REMOVE_CLASS("right");
+        };
     
     BTN_LEFT.addEventListener("click", moveLeft);
     BTN_RIGHT.addEventListener("click", moveRight);
