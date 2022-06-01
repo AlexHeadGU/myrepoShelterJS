47f623c930b39b1bd52e655946254094a9fcce6a
Alexandr <alex-davydov211@mail.ru>
2022-06-01 19:00:26 +0300

    fix: pagination

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -117,10 +117,15 @@ const showNextCards = () => {
   BTN_SELECTED.textContent = numberPage.toString();
   for(let j = 0; j < petsForPagination[numberPage].length; j++){
     let img = CARDS[j].querySelector(".pet-img");
-    let p = CARDS[j].querySelector(".pet-name");
+    // let p = CARDS[j].querySelector(".pet-name");
     img.src = `../../assets/images/.png`;
     img.alt = petsForPagination[numberPage][j].name;
-    p.innerHTML = petsForPagination[numberPage][j].name;
+    // p.innerHTML = petsForPagination[numberPage][j].name;
+  }
+
+  for(let j = 0; j < petsForPagination[numberPage+1].length; j++){
+    let p = CARDS[j].querySelector(".pet-name");
+    p.innerHTML = petsForPagination[numberPage+1][j].name;
   }
 }
 
@@ -128,15 +133,27 @@ const petsSubsequence = () => {
 
   for(let i = 0; i < 6; i++){
     existsIndex = pets;
-    // [[{"1":1},{"2":2},{"3":3},{"4":4},{"5":5},{"6":6},{"7":7},{"8":8}],
-    // [{"9":9},{"10":10},{"11":11},{"12":12},{"13":13},{"14":14},{"15":15},{"16":16}],
-    // [{"17":17},{"18":18},{"19":19},{"20":20},{"21":21},{"22":22},{"23":23},{"24":24}],
-    // [{"25":25},{"26":26},{"27":27},{"28":28},{"29":29},{"30":30},{"31":31},{"32":32}],
-    // [{"33":33},{"34":34},{"35":35},{"36":36},{"37":37},{"38":38},{"39":39},{"40":40}],
-    // [{"41":41},{"42":42},{"43":43},{"44":44},{"45":45},{"46":46},{"47":47},{"48":48}]];
     shuffle(existsIndex);
-    petsForPagination.push(existsIndex);  
-  }
+    const oneMoreShuffle = () => {
+      if(i > 0 && (existsIndex[0].name != petsForPagination[petsForPagination.length - 2].name || existsIndex[0].name != petsForPagination[petsForPagination.length - 1].name || existsIndex[1].name != petsForPagination[petsForPagination.length - 1].name)){
+        for(let j = 0; j < existsIndex.length; j++){
+          console.log("хорошо > 0");
+          petsForPagination.push(existsIndex[j]); 
+        }
+      }else if(i === 0){
+        for(let j = 0; j < existsIndex.length; j++){
+          console.log("хорошо = 0");
+          petsForPagination.push(existsIndex[j])
+      }
+      }else{
+        shuffle(existsIndex);
+        console.log("давай снова");
+        oneMoreShuffle();
+      }
+    }
+    oneMoreShuffle();
+}
+  console.log(petsForPagination);
   createCardsTemplate();
 }
 
